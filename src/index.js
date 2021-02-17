2a68955f0629d4604c9314c0f128c8198df051c8
stiffelbo <partygrouppl@gmail.com>
2021-02-17 21:23:16 +0100

    15.3 List Hero

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+ReactDOM.render(<App />,//new instance of App class 
+    document.getElementById('app'));
+
